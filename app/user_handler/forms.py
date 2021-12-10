94aad7a69e830124fce99d99761be9347e4a594a
Shyama <shyama.kumari.arunachalam@gmail.com>
2021-12-10 18:36:13 +0530

    remove unnecessary comments

--- app/user_handler/forms.py
+++ app/user_handler/forms.py
@@ -11,20 +11,3 @@ class DeleteUserForm(FlaskForm):
 
 class ChooseActionForm(FlaskForm):
     action = StringField('action', validators=[DataRequired('Please Choose your Action')])
-
-# from flask.wtf import Form # , RecaptchaField
-#
-# # Import Form elements such as TextField and BooleanField (optional)
-# from wtforms import TextField, PasswordField # BooleanField
-#
-# # Import Form validators
-# from wtforms.validators import Required, Email, EqualTo
-#
-#
-# # Define the login form (WTForms)
-#
-# class LoginForm(Form):
-#     email    = TextField('Email Address', [Email(),
-#                 Required(message='Forgot your email address?')])
-#     password = PasswordField('Password', [
-#                 Required(message='Must provide a password. ;-)')])
